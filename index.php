723a5c6b8ba483548035111914d4ee1fb8e452b6
poupakkg <60771085+poupakkg@users.noreply.github.com>
2020-04-29 00:19:43 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WOW! PHP starts</title>
+</head>
+<body>
+    <h1>WOW! PHP starts</h1>
+    <?php
+        echo ' Hello, World ! ';
+        echo ( 4 + 5 );
+        
+    ?>
+</body>
+</html>
